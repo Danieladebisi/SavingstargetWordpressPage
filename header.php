c6d0da7f3717960e4906ef7c50d588ccc244418c
danieladebisi <danidannyco@gmail.com>
2024-08-29 19:27:20 -0500

    "Updated header.php: restructured HTML, added container and logo elements, moved navigation inside header, and added header widgets section."

--- header.php
+++ header.php
@@ -7,10 +7,28 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<header>
-    <h1><?php bloginfo( 'name' ); ?></h1>
-    <p><?php bloginfo( 'description' ); ?></p>
+<header class="site-header">
+    <div class="container">
+        <div class="logo">
+            <?php if ( has_custom_logo() ) : ?>
+                <?php the_custom_logo(); ?>
+            <?php else : ?>
+                <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+            <?php endif; ?>
+        </div>
+        <nav class="main-navigation">
+            <?php
+            wp_nav_menu( array(
+                'theme_location' => 'primary',
+                'container' => false,
+                'menu_class' => 'nav-menu',
+            ) );
+            ?>
+        </nav>
+        <div class="header-widgets">
+            <?php if ( is_active_sidebar( 'header-widgets' ) ) : ?>
+                <?php dynamic_sidebar( 'header-widgets' ); ?>
+            <?php endif; ?>
+        </div>
+    </div>
 </header>
-<nav>
-    <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-</nav>
